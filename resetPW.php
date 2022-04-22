ef571b426ceb4fb05af67ab2cd75c9cc9dc2abaa
Jente Wauman <jente.wauman@gmail.com>
2022-04-22 11:22:05 +0200

    feat: add html resetPW

---
+++ resetPW.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MoreTechTips</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="netflixLogin">
+        <div class="form form--login">
+            <form action="" method="post">
+                <h2 form__title>reset password</h2>
+                <div class="form__error">
+                    <p>
+                        You can reset your password here. If you leave this page, you'll have to request a new password reset.
+                    </p>
+                </div>
+                <div class="form__field">
+                    <label for="Password">new password</label>
+                    <input type="password" name="password">
+                </div>
+                <div class="form__field">
+                    <label for="Password">confirm password</label>
+                    <input type="password" name="password">
+                </div>
+
+                <div class="form__field">
+                    <input type="submit" value="Reset" class="btn btn--primary">
+
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
