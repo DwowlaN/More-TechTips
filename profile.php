b05fd2b98009febdb8702e8fb4ae1d4c37047b59
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-04-27 20:03:23 +0200

    Adds profile.php and adds body tags

---
+++ profile.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MoreTechTips</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<body>
+    <div>
+        <p>This is your profile page</p>
+    </div>
+</body>
