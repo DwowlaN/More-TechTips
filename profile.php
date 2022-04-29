d277ef0eca06771c00b6c725f7991cc96f3a6be0
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-04-29 11:23:13 +0200

    adds some php. (connection to db and a session start thing)

--- profile.php
+++ profile.php
@@ -1,5 +1,16 @@
 <?php 
-
+    session_start();
+     // If the user is not logged in redirect to the login page...
+    if (!isset($_SESSION['loggedin'])) {
+         header('Location: index.php');
+         exit;
+    }
+    //connect to MySQL db
+    $mysql_hostname = "localhost";
+    $mysql_user = "username";
+    $mysql_password = "password";
+    $mysql_database = "moretechtips";
+    $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 
 ?>
 <head>
