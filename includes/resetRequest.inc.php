ad681ae7f932c61dda9a8003fdabea212c6e5eeb
Jente Wauman <jente.wauman@gmail.com>
2022-04-30 15:22:24 +0200

    feat: delete  user token from database

--- includes/resetRequest.inc.php
+++ includes/resetRequest.inc.php
@@ -7,7 +7,26 @@ if (isset($_POST["resetSubmit"])) {
     $url = "http://localhost/More-TechTips/resetPW.php?selector=" . $selector . "&validator= " . bin2hex($token);
 
     $expires = date("U") + 86400; 
+
+    require 'DB.inc.php';
+
+    $userEmail = $_POST["email"];
+
+    $sql = "DELETE FROM pwreset WHERE pwRmail=?";
+    $stmnt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmnt,$sql)){
+        echo "there was an error";
+        exit();
+    }
+    else{
+
+        mysqli_stmt_bind_param($stmt, "s", $userEmail);
+        mysqli_stmt_execute($stmt);
+    }
+
 } 
+
+
 else{
     header("location ../index.php");
 }
