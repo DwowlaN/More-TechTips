221377518b1654da3f1b8bf5fbd30e4d599c0c85
Jente Wauman <jente.wauman@gmail.com>
2022-05-18 17:54:07 +0200

    feat: check for email error

--- includes/resetPW.inc.php
+++ includes/resetPW.inc.php
@@ -21,7 +21,45 @@ require 'DB.inc.php';
 
 
 $sql = "SELECT * FROM pwdReset WHERE pwRselector=? AND pwRexpire >=? ";
+$stmnt = mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($stmnt,$sql)){
+    echo "There was an error";
+    exit();
+}
+else{
 
+    mysqli_stmt_bind_param($stmnt, "s", $selector, $currentDate);
+    mysqli_stmt_execute($stmnt);
+
+    $result = mysqli_stmt_get_result($stmnt);
+    if (!$row = mysqli_fetch_assoc($result)){
+        echo "please re-submit your reset request";
+        exit();
+    }
+    else{
+        $tokenBin = hex2bin($validator);
+        $tokenCheck = password_verify($tokenBin, $row["pwRtoken"]);
+
+        if($tokenCheck === false){
+            echo "please re-submit your reset request.";
+            exit();
+        } elseif($tokenCheck === true){
+        $tokenEmail = $row['pwRmail'];
+
+        $sql = "SELECT * FROM user WHERE email=?;";
+        $stmnt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmnt,$sql)){
+            echo "There was an error";
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmnt, "s", $tokenEmail);
+            mysqli_stmt_execute($stmnt);
+        }
+
+        }
+    }
+}
 
 }
 
